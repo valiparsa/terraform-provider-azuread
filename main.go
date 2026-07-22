7e9c6687b209f579eb9133368a5483a84dc62be5
Parsa Vali <parsa.vali@ista.com>
2026-07-22 16:39:12 +0200

    fix: allow testing the provider locally

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/valiparsa/terraform-provider-azuread/internal/provider"
@@ -18,9 +19,18 @@ func main() {
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
+	// The provider's registry address doubles as the reattach key that Terraform
+	// matches against a configuration's required_providers "source". Allow it to be
+	// overridden (e.g. to "registry.terraform.io/hashicorp/azuread") for local
+	// debugging against configs that reference the upstream source.
+	providerAddr := "registry.terraform.io/GPKbdZZb/forked-azuread"
+	if v := os.Getenv("PROVIDER_ADDR"); v != "" {
+		providerAddr = v
+	}
+
 	opts := &plugin.ServeOpts{
-		Debug:        false,
-		ProviderAddr: "registry.terraform.io/GPKbdZZb/forked-azuread",
+		Debug:        debug,
+		ProviderAddr: providerAddr,
 		ProviderFunc: provider.AzureADProvider,
 	}
 
